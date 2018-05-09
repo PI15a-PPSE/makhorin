668ecc8f7899bfdae03a0a5be897c133e837882c
0x7FFF <gvozdevik@gmail.com>
2018-05-09 18:53:13 +0300

    Implementing a turn-based gameplay mechanic

--- src/index.js
+++ src/index.js
@@ -17,14 +17,19 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setStaate({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setStaate(
+      {squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
   }
+
     renderSquare(i) {
       return (<Square 
       value={this.state.squares[i]}
@@ -34,7 +39,7 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
