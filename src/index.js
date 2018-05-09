1f0b8f623965eb91b4aa619e3fdc3e5d86f9004c
0x7FFF <gvozdevik@gmail.com>
2018-05-09 13:44:41 +0300

    Added a square class, fixed a missing instance of it inside the Board

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  
 class Board extends React.Component {
     renderSquare(i) {
       return <Square />;
